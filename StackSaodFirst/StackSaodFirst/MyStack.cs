306545def3e70472de568b3a349de066cc32387a
tokarbpekarb <50523349+tokarbpekarb@users.noreply.github.com>
2022-03-02 20:11:22 +0300

    Add files via upload

---
+++ StackSaodFirst/StackSaodFirst/MyStack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackSaodFirst
+{
+    class MyStack<T>
+    {
+        private T[] array;
+        private int count;
+        private int length = 10;
+
+        public int Size { get => length;}
+        public bool IsEmpty {
+            get { return count == 0; } 
+        }
+
+        public MyStack()
+        {
+            array = new T[length];
+        }
+        public void Push(T item)
+        {
+            if(count < length)
+            {
+                array[count++] = item;
+            }
+            else
+            {
+                throw new Exception("StackOverflow");
+            }
+        }
+
+        public T Pop()
+        {
+            if (!IsEmpty)
+            {
+                T item = array[--count];
+                array[count] = default(T);
+                return item;
+            }
+            else
+            {
+                return default(T);
+                //throw new Exception("Stack is empty!");
+            }
+        }
+
+        public T Peek()
+        {
+            if (!IsEmpty)
+                return array[count - 1];
+            else
+            {
+                return default(T);
+                //throw new Exception("Stack is empty!");
+            }
+        }
+    }
+}
---
+++ StackSaodFirst/StackSaodFirst/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackSaodFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestStackMethod();
+            Console.ReadLine();
+        }
+        public static void TestStackMethod()
+        {
+            MyStack<string> stack = new MyStack<string>();
+
+            Console.WriteLine(stack.Size);
+            Console.WriteLine(stack.Peek());
+
+            stack.Push("first");
+            stack.Push("second");
+
+            Console.WriteLine(stack.IsEmpty);
+            Console.WriteLine(stack.Size);
+
+            stack.Push("third");
+            stack.Push("fourth");
+
+            while (!stack.IsEmpty)
+            {
+                Console.WriteLine(stack.Peek());
+                stack.Pop();
+            }
+
+            stack.Pop();
+            Console.WriteLine(stack.IsEmpty);
+        }
+    }
+}
